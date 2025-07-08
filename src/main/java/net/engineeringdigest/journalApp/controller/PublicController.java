f0eacc4e981db9c3ba7a6d7f6989b69b66935785
Manish <guptamanish181992@gmail.com>
2025-07-08 05:44:33 -0700

    wip

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -3,6 +3,8 @@ package net.engineeringdigest.journalApp.controller;
 import net.engineeringdigest.journalApp.entity.UserEntry;
 import net.engineeringdigest.journalApp.model.UserModel;
 import net.engineeringdigest.journalApp.service.UserService;
+import net.engineeringdigest.journalApp.util.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,11 +22,23 @@ public class PublicController {
 
 
     @PostMapping("/createUser")
-    public void createNewUser(@RequestBody UserModel user) {
-        // 1. Convert UserModel to UserEntity
+    public ResponseEntity<ApiResponse<String>> createNewUser(@RequestBody UserModel user) {
+        UserEntry existingUser = userService.getUserByUserName(user.getUserName());
+        if (existingUser != null) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponse<>(false, "User already exists", null));
+        }
         UserEntry userEntityObject = new UserEntry();
         userEntityObject.setUserName(user.getUserName());
         userEntityObject.setPassword(user.getPassword());
         userService.createNewUser(userEntityObject);
+        return ResponseEntity.ok(
+                new ApiResponse<>(true, "User created successfully", null)
+        );
+    }
+
+    @GetMapping("/github-users")
+    public ResponseEntity<Object> getGitHubUsers() {
+        return ResponseEntity.ok(userService.getGitHubUsers());
     }
 }
--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -3,8 +3,11 @@ package net.engineeringdigest.journalApp.service;
 import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.UserEntry;
 import net.engineeringdigest.journalApp.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -13,13 +16,20 @@ import java.util.List;
 @Slf4j
 public class UserService {
 
+    @Value("${external.github.base-url}")
+    private String BASE_URL_GITHUB;
+
     private final UserRepo userRepo;
+    private final RestTemplate restTemplate;
 
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepo userRepo,
+                       PasswordEncoder passwordEncoder,
+                       RestTemplate restTemplate) {
         this.userRepo = userRepo;
         this.passwordEncoder = passwordEncoder;
+        this.restTemplate = restTemplate;
     }
 
     public void createNewUser(UserEntry user) {
@@ -36,6 +46,13 @@ public class UserService {
 
     public UserEntry getUserByUserName(String userName) {
         return userRepo.findByUserName(userName);
+    }
+
+    public Object getGitHubUsers() {
+        String githubUsersEndpoint = "users";
+        String url = BASE_URL_GITHUB + githubUsersEndpoint;
+        ResponseEntity<Object> response = restTemplate.getForEntity(url, Object.class);
+        return response.getBody();
     }
 
 }
